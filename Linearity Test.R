46d3644de0ce56272741a5453d6a6d5fd15c5e1e
NehemiahDaGoat <50632115+NehemiahDaGoat@users.noreply.github.com>
2019-06-12 12:54:59 -0400

    Linearity Upload

---
+++ Linearity Test.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+
+#Uploading Picarro Table 1 and Organizing Variables
+Picarro1<-read.table("C:/Users/Nehemiah/Documents/CarbonShed Lab/Data/Picarro0611_1753_1823.txt", header=TRUE)
+view(Picarro1)
+Time<-Picarro1$TIME
+CO2<-Picarro1$X12CO2
+
+#Separating Time and CO2 into
+Time[1277:1376]
